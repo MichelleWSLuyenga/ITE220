17d2b1763a0ddd2a5b5df1ee358ec218a5b79fb7
HADES <216110108@students.stamford.edu>
2017-04-06 15:56:39 +0700

    Forgot

---
+++ Week3/js/shop.js
@@ -0,0 +1,14 @@
+var customerName = "Raisin";
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+
+var customer =  document.getElementById("name");
+customer.textContent = customerName;
+var price = document.getElementById("total");
+price.textContent = total;
